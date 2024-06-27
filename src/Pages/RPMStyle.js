a74b111dbb85b5edbe877f0fe219953cdfc6014a
SandyNabil <sandynabil257@gmail.com>
2024-06-27 18:47:37 +0300

    Update rpm

--- src/Pages/RPMStyle.js
+++ src/Pages/RPMStyle.js
@@ -49,9 +49,9 @@ const ctx = chart.ctx;
 const gradient = ctx.createLinearGradient(0, 0, chart.width, 0);
 
 readings.forEach((reading, index) => {
-const color = getColor(reading.value, minValue, maxValue, avgValue);
-const stopPosition = index / (readings.length - 1);
-gradient.addColorStop(stopPosition, color);
+    const color = getColor(reading.value, minValue, maxValue, avgValue);
+    const stopPosition = index / (readings.length - 1);
+    gradient.addColorStop(stopPosition, color);
 });
 
 chart.data.datasets[0].borderColor = gradient;
@@ -65,28 +65,28 @@ new Date(reading.createdDate).toLocaleTimeString()
 ),
 datasets: [
 {
-label: sensorName,
-data: readings.map((reading) => reading.value),
-fill: false,
-borderColor: (context) => {
+    label: sensorName,
+    data: readings.map((reading) => reading.value),
+    fill: false,
+    borderColor: (context) => {
     const index = context.dataIndex;
     const value = context.dataset.data[index];
     return getColor(value, minValue, maxValue, avgValue);
-},
-backgroundColor: "rgba(0, 0, 0, 0.1)",
-pointRadius: 5,
-pointBackgroundColor: (context) => {
+    },
+    backgroundColor: "rgba(0, 0, 0, 0.1)",
+    pointRadius: 5,
+    pointBackgroundColor: (context) => {
     const index = context.dataIndex;
     const value = context.dataset.data[index];
     return getColor(value, minValue, maxValue, avgValue);
-},
-pointShadowColor: (context) => {
+    },
+    pointShadowColor: (context) => {
     const index = context.dataIndex;
     const value = context.dataset.data[index];
     return getColor(value, minValue, maxValue, avgValue);
-},
-pointShadowBlur: 10,
-cubicInterpolationMode: "monotone",
+    },
+    pointShadowBlur: 10,
+    cubicInterpolationMode: "monotone",
 },
 ],
 };
@@ -94,25 +94,24 @@ cubicInterpolationMode: "monotone",
 const chartOptions = {
 scales: {
 y: {
-suggestedMin: minValue - 5,
-suggestedMax: maxValue + 5,
-title: {
+    suggestedMin: minValue - 5,
+    suggestedMax: maxValue + 5,
+    title: {
     display: true,
     text: `${sensorName} (${unit})`,
-},
+    },
 },
 },
 plugins: {
 tooltip: {
-callbacks: {
+    callbacks: {
     label: function (context) {
-    const value = context.raw;
-    const date = new Date(readings[context.dataIndex].createdDate);
-    const dateString = date.toLocaleDateString("en-GB");
-    // const timeString = date.toLocaleTimeString();
-    return `${sensorName}: ${value} ${unit} (${dateString})`;
+        const value = context.raw;
+        const date = new Date(readings[context.dataIndex].createdDate);
+        const dateString = date.toLocaleDateString("en-GB");
+        return `${sensorName}: ${value} ${unit} (${dateString})`;
     },
-},
+    },
 },
 },
 };
@@ -146,234 +145,235 @@ width: "33.33%", // Equal width for all columns
 };
 
 return (
-<div style={{ padding: "20px", backgroundColor: "#15131e" }}>
-<h1
-style={{
-    textAlign: "center",
-    color: "#f0c302",
-    fontSize: "5vh",
-    marginBottom: "3vh",
-}}
->
-{sensorName}
-</h1>
-<p
-style={{
-    color: "#e8e7e9",
-    margin: "1vw",
-    fontStyle: "italic",
-    fontSize: "18px",
-}}
->
-{description}
-</p>
-
-<div
-style={{
-    display: "flex",
-    justifyContent: "center",
-    marginBottom: "20px",
-    backgroundColor: "rgba(34,34,46,0.6)",
-    padding: "20px",
-    boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
-    width: "100%",
-    flexDirection: "column",
-    alignItems: "center",
-}}
->
-<GaugeChart
-    id="gauge-chart"
-    nrOfLevels={30}
-    percent={latestReading / 3125} // Adjust the max value as needed
-    textColor="#ffffff"
-/>
-</div>
-
-<div
-style={{
-    display: "flex",
-    flexDirection: isSmallScreen ? "column" : "row",
-    gap: "20px",
-    margin: "20px 0",
-}}
->
-<div
-    style={{
-    flex: isSmallScreen ? "1" : "3",
-    backgroundColor: "rgba(34,34,46,0.6)",
-    padding: "20px",
-    boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
-    width: isSmallScreen ? "100%" : "70%",
-    }}
->
-    <Line ref={chartRef} data={chartData} options={chartOptions} />
-</div>
-<div
-    style={{
-    flex: isSmallScreen ? "1" : "1",
-    backgroundColor: "rgba(34,34,46,0.6)",
-    padding: "20px",
-    boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
-    width: isSmallScreen ? "100%" : "27%",
-    display: "flex",
-    flexDirection: isSmallScreen ? "column" : "column",
-    justifyContent: "space-between",
-    }}
->
+  <div style={{ padding: "20px", backgroundColor: "#15131e" }}>
+    <h1
+      style={{
+        textAlign: "center",
+        color: "#f0c302",
+        fontSize: "5vh",
+        marginBottom: "3vh",
+      }}
+    >
+      {sensorName}
+    </h1>
     <p
-    style={{
+      style={{
         color: "#e8e7e9",
-        fontSize: isSmallScreen ? "2.5vh" : "3vh",
-    }}
+        margin: "1vw",
+        fontStyle: "italic",
+        fontSize: "18px",
+      }}
     >
-    Min Value:{" "}
-    <span
-        style={{
-        color: getColor(minValue, minValue, maxValue, avgValue),
-        }}
-    >
-        {minValue}
-    </span>{" "}
-    {unit}
-    </p>
-    <p
-    style={{
-        color: "#e8e7e9",
-        fontSize: isSmallScreen ? "2.5vh" : "3vh",
-    }}
-    >
-    Avg Value:{" "}
-    <span
-        style={{
-        color: getColor(avgValue, minValue, maxValue, avgValue),
-        }}
-    >
-        {avgValue}
-    </span>{" "}
-    {unit}
-    </p>
-    <p
-    style={{
-        color: "#e8e7e9",
-        fontSize: isSmallScreen ? "2.5vh" : "3vh",
-    }}
-    >
-    Max Value:{" "}
-    <span
-        style={{
-        color: getColor(maxValue, minValue, maxValue, avgValue),
-        }}
-    >
-        {maxValue}
-    </span>{" "}
-    {unit}
+      {description}
     </p>
 
-    <p
-    style={{
-        color: "#e8e7e9",
-        fontSize: isSmallScreen ? "2.5vh" : "3vh",
-    }}
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        marginBottom: "20px",
+        backgroundColor: "rgba(34,34,46,0.6)",
+        padding: "20px",
+        boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+        width: "100%",
+        flexDirection: "column",
+        alignItems: "center",
+      }}
     >
-    Current Reading:{" "}
-    <span
-        style={{
-        color: getColor(latestReading, minValue, maxValue, avgValue),
-        }}
-    >
-        {latestReading}
-    </span>{" "}
-    {unit}
-    </p>
-</div>
-</div>
+      <GaugeChart
+        id="gauge-chart"
+        nrOfLevels={30}
+        percent={latestReading / maxValue} // Adjust the max value as needed
+        textColor={getColor(latestReading, minValue, maxValue, avgValue)}
+        formatTextValue={(value) => `${latestReading} RPM`}
+      />
+    </div>
 
-<div>
-<label style={{ color: "#e8e7e9" }}>
-    Select Date:
-    <input
-    type="date"
-    name="selectedDate"
-    value={selectedDate}
-    onChange={handleDateChange}
-    style={{ marginLeft: "10px" }}
-    />
-</label>
-<div
-    style={{
-    marginTop: "20px",
-    maxHeight: "400px",
-    overflowY: "auto",
-    }}
->
-    <TableContainer
-    sx={{
-        borderRadius: 2,
-        bgcolor: "rgba(34,34,46,0.6)",
-        p: 2,
-    }}
+    <div
+      style={{
+        display: "flex",
+        flexDirection: isSmallScreen ? "column" : "row",
+        gap: "20px",
+        margin: "20px 0",
+      }}
     >
-    <Table sx={{ minWidth: 650 }} aria-label="simple table">
-        <TableHead>
-        <TableRow sx={borderBottomStyle}>
-            <TableCell align="center" sx={tableHeadCellStyle}>
-            Value ({unit})
-            </TableCell>
-            <TableCell align="center" sx={tableHeadCellStyle}>
-            Date
-            </TableCell>
-            <TableCell align="center" sx={timeHeaderCellStyle}>
-            Time
-            </TableCell>
-        </TableRow>
-        </TableHead>
-        <TableBody>
-        {selectedDateReadings.length > 0 ? (
-            selectedDateReadings.map((reading, index) => (
-            <TableRow key={index} sx={borderBottomStyle}>
-                <TableCell
-                align="center"
-                sx={{
-                    ...tableBodyCellStyle,
-                    color: getColor(
-                    reading.value,
-                    minValue,
-                    maxValue,
-                    avgValue
-                    ),
-                    fontSize: "17px",
-                    borderRight: "2px solid #454545",
-                }}
-                >
-                {reading.value}
+      <div
+        style={{
+          flex: isSmallScreen ? "1" : "3",
+          backgroundColor: "rgba(34,34,46,0.6)",
+          padding: "20px",
+          boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+          width: isSmallScreen ? "100%" : "70%",
+        }}
+      >
+        <Line ref={chartRef} data={chartData} options={chartOptions} />
+      </div>
+      <div
+        style={{
+          flex: isSmallScreen ? "1" : "1",
+          backgroundColor: "rgba(34,34,46,0.6)",
+          padding: "20px",
+          boxShadow: "0 4px 8px rgba(0,0,0,0.2)",
+          width: isSmallScreen ? "100%" : "27%",
+          display: "flex",
+          flexDirection: isSmallScreen ? "column" : "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <p
+          style={{
+            color: "#e8e7e9",
+            fontSize: isSmallScreen ? "2.5vh" : "3vh",
+          }}
+        >
+          Min Value:{" "}
+          <span
+            style={{
+              color: getColor(minValue, minValue, maxValue, avgValue),
+            }}
+          >
+            {minValue}
+          </span>{" "}
+          {unit}
+        </p>
+        <p
+          style={{
+            color: "#e8e7e9",
+            fontSize: isSmallScreen ? "2.5vh" : "3vh",
+          }}
+        >
+          Avg Value:{" "}
+          <span
+            style={{
+              color: getColor(avgValue, minValue, maxValue, avgValue),
+            }}
+          >
+            {avgValue}
+          </span>{" "}
+          {unit}
+        </p>
+        <p
+          style={{
+            color: "#e8e7e9",
+            fontSize: isSmallScreen ? "2.5vh" : "3vh",
+          }}
+        >
+          Max Value:{" "}
+          <span
+            style={{
+              color: getColor(maxValue, minValue, maxValue, avgValue),
+            }}
+          >
+            {maxValue}
+          </span>{" "}
+          {unit}
+        </p>
+
+        <p
+          style={{
+            color: "#e8e7e9",
+            fontSize: isSmallScreen ? "2.5vh" : "3vh",
+          }}
+        >
+          Current Reading:{" "}
+          <span
+            style={{
+              color: getColor(latestReading, minValue, maxValue, avgValue),
+            }}
+          >
+            {latestReading}
+          </span>{" "}
+          {unit}
+        </p>
+      </div>
+    </div>
+
+    <div>
+      <label style={{ color: "#e8e7e9" }}>
+        Select Date:
+        <input
+          type="date"
+          name="selectedDate"
+          value={selectedDate}
+          onChange={handleDateChange}
+          style={{ marginLeft: "10px" }}
+        />
+      </label>
+      <div
+        style={{
+          marginTop: "20px",
+          maxHeight: "400px",
+          overflowY: "auto",
+        }}
+      >
+        <TableContainer
+          sx={{
+            borderRadius: 2,
+            bgcolor: "rgba(34,34,46,0.6)",
+            p: 2,
+          }}
+        >
+          <Table sx={{ minWidth: 650 }} aria-label="simple table">
+            <TableHead>
+              <TableRow sx={borderBottomStyle}>
+                <TableCell align="center" sx={tableHeadCellStyle}>
+                  Value ({unit})
                 </TableCell>
-                <TableCell align="center" sx={tableBodyCellStyle}>
-                {new Date(reading.createdDate).toLocaleDateString(
-                    "en-GB"
-                )}
+                <TableCell align="center" sx={tableHeadCellStyle}>
+                  Date
                 </TableCell>
-                <TableCell align="center" sx={tableBodyCellStyle}>
-                {new Date(reading.createdDate).toLocaleTimeString()}
+                <TableCell align="center" sx={timeHeaderCellStyle}>
+                  Time
                 </TableCell>
-            </TableRow>
-            ))
-        ) : (
-            <TableRow>
-            <TableCell
-                colSpan={3}
-                align="center"
-                sx={{ p: 2, fontSize: "2.5vh" }}
-            >
-                No readings available for selected date.
-            </TableCell>
-            </TableRow>
-        )}
-        </TableBody>
-    </Table>
-    </TableContainer>
-</div>
-</div>
-</div>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {selectedDateReadings.length > 0 ? (
+                selectedDateReadings.map((reading, index) => (
+                  <TableRow key={index} sx={borderBottomStyle}>
+                    <TableCell
+                      align="center"
+                      sx={{
+                        ...tableBodyCellStyle,
+                        color: getColor(
+                          reading.value,
+                          minValue,
+                          maxValue,
+                          avgValue
+                        ),
+                        fontSize: "17px",
+                        borderRight: "2px solid #454545",
+                      }}
+                    >
+                      {reading.value}
+                    </TableCell>
+                    <TableCell align="center" sx={tableBodyCellStyle}>
+                      {new Date(reading.createdDate).toLocaleDateString(
+                        "en-GB"
+                      )}
+                    </TableCell>
+                    <TableCell align="center" sx={tableBodyCellStyle}>
+                      {new Date(reading.createdDate).toLocaleTimeString()}
+                    </TableCell>
+                  </TableRow>
+                ))
+              ) : (
+                <TableRow>
+                  <TableCell
+                    colSpan={3}
+                    align="center"
+                    sx={{ p: 2, fontSize: "2.5vh" }}
+                  >
+                    No readings available for selected date.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+    </div>
+  </div>
 );
 };
 
